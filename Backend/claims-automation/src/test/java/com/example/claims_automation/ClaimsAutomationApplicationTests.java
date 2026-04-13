7740ddc1ef51de66977b364cf30938a5615927b4
Saurabh Kumar <0131cs231177@jnctbhopal.ac.in>
2026-04-13 12:00:13 +0530

    Initial commit

---
+++ Backend/claims-automation/src/test/java/com/example/claims_automation/ClaimsAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.claims_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
